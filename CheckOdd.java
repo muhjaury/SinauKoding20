b20f7ceaf557d030fa209ec39c69feee69ea306c
Muhammad Jaury <51047545+muhjaury@users.noreply.github.com>
2022-06-02 14:05:41 +0800

    Add files via upload

---
+++ CheckOdd.java
@@ -0,0 +1,24 @@
+package com;
+
+public class CheckOdd {
+    public static void main(String[] args) {
+        int awal = 1;
+        int akhir = 50;
+        int odd=0;
+        for(;awal<=akhir;awal++){
+            odd=0;
+            for(int check = 1;check<=awal;check++){
+                if(awal%check==0){
+                    odd+=1;
+                }
+            }
+
+            if(odd==2){
+                System.out.println(awal+" bilangan prima");
+            }
+            else{
+                System.out.println(awal+" bukan bilangan prima");
+            }
+        }
+    }
+}
