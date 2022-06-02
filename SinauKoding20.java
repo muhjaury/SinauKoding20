c3abd2713fca294a90b3229ca6193e86bdcdfd73
Muhammad Jaury <51047545+muhjaury@users.noreply.github.com>
2022-06-02 14:35:04 +0800

    Update SinauKoding20.java

--- SinauKoding20.java
+++ SinauKoding20.java
@@ -8,10 +8,13 @@ public class SinauKoding20 {
 //        int Angka = scan.nextInt();
         int x=1;
         while(x<51){
-            if(x%3==0){
+            if(x%3==0 && x%5==0){
+                System.out.println("Angka "+x+" habis dibagi 3 dan 5");
+            }
+            else if(x%3==0){
                 System.out.println("Angka "+x+" habis dibagi 3");
             }
-            if(x%5==0){
+            else if(x%5==0){
                 System.out.println("Angka "+x+" habis dibagi 5");
             }
             x++;
